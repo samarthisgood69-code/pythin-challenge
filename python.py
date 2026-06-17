605abc2323b23883aa2b85959e5abb656081f54c
samarthisgood69 <samarthisgood69@gmail.com>
2026-06-17 11:17:04 +0530

    initial commit

---
+++ python.py
@@ -0,0 +1,17 @@
+x = 9 
+v = 4 
+g = 7
+h = 9 
+f = (v+x)*g/h
+print("value of (v+x)*g/h", f)
+
+
+name = "ani" 
+age = 9 
+if name == "ani"or name =="ana" and age >= 15:
+    print("Hello! welcome")
+else:
+    print("good bye!")
+
+
+
