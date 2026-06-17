76974e9aed473be92c9e82f33d5a55aa203d2a81
samarthisgood69 <samarthisgood69@gmail.com>
2026-06-17 11:39:55 +0530

    initial commit

---
+++ mean.py
@@ -0,0 +1,14 @@
+mean1 = 98
+wrong_number = 47
+correct_number = 56
+total_number = 152
+#sum of 152 numbers 
+sum = mean1*total_number
+print("the sum of 152 numbers: ",sum)
+# correct sum of these numbers
+num2= sum-((wrong_number)- (correct_number))
+print("sum-((wrong_number)-(correct_number)): ", num2)
+#the correct mean
+mean2 = num2/total_number
+print(mean2)
+
